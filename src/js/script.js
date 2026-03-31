b11696521b0e471503964ca9782ad219da0c896a
Matheus Cunha <cunha.matheus2008@gmail.com>
2026-03-31 10:12:51 -0300

    feat: adicionando exibição do cadastro

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
 let nome = prompt("Digite seu nome: ");
-let nome_vinho = prompt("Insira o nomne do vinho: ");
+let nome_vinho = prompt("Insira o nome do vinho: ");
 let tipo_vinho = prompt("Insira o tipo do vinho: ");
 let safra_vinho = prompt("Insira o ano da safra do vinho: ");
-let qtde_vinho = parseInt(prompt("Digite a quantidade em estoque: "))
+let qtde_vinho = parseInt(prompt("Digite a quantidade em estoque: "));
+
+alert("Cadastro realizado! Veja os detalhes no console.");
+console.log(`Vinho cadastrado por ${nome}.`);
+console.log(`Vinho: ${nome_vinho}.
+Tipo: ${tipo_vinho}.
+Ano: ${safra_vinho}.
+Quantidade em estoque: ${qtde_vinho}.`);
