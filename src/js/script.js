0a455943cb86242d9f4a94dd8677329e3b6402ad
felipericardo12 <felipeeemoreiraaa12@gmail.com>
2026-03-31 09:52:02 -0300

    Feat: Iniciando prompt do cadastro

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite seu nome: ");
+let nome_vinho = prompt("Insira o nomne do vinho: ");
+let tipo_vinho = prompt("Insira o tipo do vinho: ");
+let safra_vinho = prompt("Insira o ano da safra do vinho: ");
+let qtde_vinho = parseInt(prompt("Digite a quantidade em estoque: "))
